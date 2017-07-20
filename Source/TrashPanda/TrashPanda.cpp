11dc6dd881ca656c297a3902a94e4b47c8a92f8b
grejaili <gabrielrejaili@gmail.com>
2017-07-20 01:17:44 -0700

    hey

---
+++ Source/TrashPanda/TrashPanda.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TrashPanda.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TrashPanda, "TrashPanda" );
---
+++ Source/TrashPanda/TrashPandaGameState.h
@@ -0,0 +1,19 @@
+// All Rights Reserved for Students Graduating TFS Summer 2017
+
+#pragma once
+
+#include "GameFramework/GameStateBase.h"
+#include "TrashPandaGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRASHPANDA_API ATrashPandaGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
